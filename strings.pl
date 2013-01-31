e2edacc2d7656f9def3927954096fff879e0d890
shakun <singh.shakun92@gmail.com>
2013-01-31 11:57:31 +0530

    adding strings.pl

---
+++ strings.pl
@@ -0,0 +1,20 @@
+#!c:/perl/bin
+
+use strict;
+use warnings;
+
+sub main
+{
+  my $s= "this is string";
+  my $s1= 'this is string';
+  my $s2= qq<this is quoted string>;
+  my $s3= q<this is single q>;
+
+}
+
+sub error
+{
+  my $e = shift || "unknown error";
+  print ("$0, $e\n");
+  exit 0;
+} 
