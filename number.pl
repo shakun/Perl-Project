be7bbad2b472e79948f1706122d2173ac86e91b0
shakun <singh.shakun92@gmail.com>
2013-01-31 11:12:18 +0530

    adding number.pl

---
+++ number.pl
@@ -0,0 +1,20 @@
+#!c:/perl/bin
+
+use strict;
+use warnings;
+
+sub main
+{
+  my $integer=23;
+  my $float=21.8;
+  my $binary=0b0011;
+  my $hex=0xfacd;
+  my $octal=0567; 
+ }
+
+sub error
+{
+  my $e = shift || "unknown error";
+  print ("$0, $e\n");
+  exit 0;
+} 
